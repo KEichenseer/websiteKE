5ba5ef8239c83e580ca5f619b1d07d10d2a66187
Kilian Eichenseer <kilian.eichenseer@gmail.com>
2022-03-26 21:40:08 +0000

    starting post on adaptive proposals for Metropolis algorithm

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:D:\data\websiteKE\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
+  // ns-hugo:D:\Github\websiteKE\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
